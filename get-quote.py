2be757fba7056b2b5c14378ba6439d4498d529b1
root <root@localhost.localdomain>
2020-05-23 16:49:31 +0530

    work done for displaying2 random lines

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,22 @@
 import random
+
 def head():
   # print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  last=13
-  rnd=random.randint(0,last)
-
-  print(quotes[rnd])
-
+  last=18
+  
+  #rnd=random.randint(0,last)
+  #print(quotes[rnd])
+  print(random.sample(quotes,2))
+  
 if __name__== "__main__":
-  head()
+ head()
+
+
+
+
+
+
